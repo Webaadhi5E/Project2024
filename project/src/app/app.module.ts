09c25017b42277e6e2c850f057596d79b3e6a2d1
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-05-04 20:08:30 +0530

    New List enhancement

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, UpperCasePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DatePipe,
+    UpperCasePipe,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
